807e28e4966ca0be71233feb2cab15fe92ef4a3d
jdavida56 <jdavida56@hotmail.com>
2023-03-08 21:31:09 -0500

    primerasubida

---
+++ src/primeprograma/Javaintroej.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primeprograma;
+
+import java.util.Scanner;
+/**
+ *
+ * @author JDAVID
+ */
+public class Javaintroej {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner leer = new Scanner(System.in);
+        
+        System.out.println("con esta clase guardamos valores ingresado por consola en las variable");
+        
+        System.out.println("ingresa tu nombre");
+        String nombreCompleto = leer.nextLine();
+        
+        System.out.println("ingresa tu edad");
+        int edad = leer.nextInt();
+        
+        /*String nombre = "juan david";
+        String nombreCompleto;
+        String apellido = " acevedo";
+        int numero = 3;
+        int numero2 = 5;
+        double decimales = 3.0;
+        boolean siono = true;
+        char letra = 'a';
+        int resultado = 0;
+        int edad = 25;
+        
+        resultado = numero - numero2;
+        
+        nombreCompleto = nombre + apellido;
+        
+        if (siono = true) {
+            siono = false;
+        }
+                
+        decimales = numero2/numero;
+        
+        
+        System.out.println (resultado);
+        System.out.print("el valor decimal es:" + decimales);
+        System.out.println("  El estado de la variable booleana es " + siono);
+*/        
+System.out.println("Nombre y Apellido " + nombreCompleto + " y la edad es: " + edad);
+    }
+    
+}
